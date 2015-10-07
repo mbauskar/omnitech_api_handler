b162cf28c6c6a54279fc5003b0b03956c232ef2a
mbauskar <mbauskar@gmail.com>
2015-10-07 13:07:02 +0530

    removed the pass_request_to_esb method

--- api_handler/api_handler/doctype/request_log/create_log.py
+++ api_handler/api_handler/doctype/request_log/create_log.py
@@ -15,36 +15,3 @@ def request_log(method, response, request_data, error=None):
 
 def get_json(data):
     return json.loads(data)
-
-# TODO move pass_request_to_client_esb method to request_log.py ?
-def pass_request_to_client_esb(doc, method):
-    from suds.client import Client
-    url = "file:///home/makarand/Desktop/Projects/omnitech/CRM_ACCEPTANCE_MSGService.wsdl"
-    client = Client(url, cache=None)
-
-    # Request and response data
-    logged_request = json.loads(doc.request_parameters)
-    logged_response = json.loads(doc.response)
-
-    P_CRM_ID = logged_request.get("P_TRXN_NO")
-    P_CIRCUIT_NO = "17XXXXXX"
-    P_SERVICE_TYPE = "SaaS"
-    P_ACTION_TYPE = "00"
-    P_RETURN_CODE = logged_response.get("P_RETURN_CODE")
-    P_RETURN_MESG = logged_response.get("P_RETURN_MESG")
-    P_ATTRIBUTE1 = logged_request.get("P_ATTRIBUTE1") or None
-    P_ATTRIBUTE2 = logged_request.get("P_ATTRIBUTE2") or None
-    P_ATTRIBUTE3 = logged_request.get("P_ATTRIBUTE3") or None
-
-    try:
-        response = client.service.AcceptRequest(P_CRM_ID=P_CRM_ID,P_SERVICE_TYPE=P_SERVICE_TYPE,
-                                                P_CIRCUIT_NO=P_CIRCUIT_NO,P_ACTION_TYPE=P_ACTION_TYPE,
-                                                P_RETURN_CODE=P_RETURN_CODE,P_RETURN_MESG=P_RETURN_MESG,
-                                                P_ATTRIBUTE1=P_ATTRIBUTE1,P_ATTRIBUTE2=P_ATTRIBUTE2,
-                                                P_ATTRIBUTE3=P_ATTRIBUTE3)
-        # TODO check response
-        print response
-    except Exception, e:
-        frappe.db.rollback()
-        print "env\n%s\n%s"%(e,client.last_sent().str())
-        frappe.throw(str(e))
